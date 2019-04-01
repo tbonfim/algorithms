cb6f7ebd58ad52a411e89df38fb4de3714e78fdf
Thiago Bonfim <thiakau@gmail.com>
2019-04-01 15:11:05 -0700

    Adding Palindrome method

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+(function(){
+    /** Palindrome:
+     * 
+     * Given a string, returns if its palindrome or not
+     */
+   
+    var palindrome = function(str){
+        return str.length < 2 ? true : str[0] !== str[str.length-1] ? false : palindrome(str.slice(1, -1));
+    }
+})();
