34d661b8374dfbaeee6d3743c94db477246c0424
Thiago Bonfim <thiakau@gmail.com>
2019-04-02 11:38:36 -0700

    Adding mergesort

---
+++ algorithms/sort/mergesort.js
@@ -0,0 +1,28 @@
+const mergeSort = array => {
+	if (array.length < 2) return array;
+
+	const center = Math.floor(array.length / 2);
+	const left = array.slice(0, center),
+		right = array.slice(center, array.length);
+
+	return merge(mergeSort(left), mergeSort(right));
+};
+const merge = (left, right) => {
+	const result = [];
+
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) result.push(left.shift());
+		else result.push(right.shift());
+	}
+
+	while (left.length) result.push(left.shift());
+	while (right.length) result.push(right.shift());
+
+	return result;
+};
+
+// (()=>{
+//     const array = [2,3,1,4,5,2,3,5,67,13,4,5,34,45,245,3456,7,57,45,6,32,4,1,243];
+//     console.log(array.toString());
+//     console.log(' Sorted array : ', mergeSort(array).toString());
+// })();
