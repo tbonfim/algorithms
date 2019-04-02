205a14d3c6f9798cd2638b8ea8c723f3cad6499b
Thiago Bonfim <thiakau@gmail.com>
2019-04-02 10:43:39 -0700

    Adding stack

---
+++ stack.js
@@ -0,0 +1,31 @@
+class Stack {
+	constructor() {
+		this.data = [];
+	}
+	push(data) {
+		this.data.push(data);
+	}
+	pop() {
+		return this.data.pop();
+	}
+	peek() {
+		return this.data[this.data.length - 1];
+    }
+    size() {
+        return this.data.length;
+    }
+}
+
+// (function(){
+//     let stack = new Stack();
+//     console.log(' Initial size ', stack.size());
+//     stack.push(1);
+//     stack.push(2);
+//     stack.push(3);
+//     stack.push(4);
+//     console.log( ' Size ', stack.size());
+//     console.log( ' Peek ', stack.peek());
+//     console.log( ' Size ', stack.size());
+//     console.log( ' Pop ', stack.pop());
+//     console.log( ' Size ', stack.size());
+// })();
